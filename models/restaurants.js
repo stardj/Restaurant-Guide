766224d4a2bb8e2d739aef2ffbd94084041df990
Jiaotangmqd <34199729+Jiaotangmqd@users.noreply.github.com>
2018-04-18 21:12:46 +0100

    重新上传。。不小心把分支删了
    增加图片上传/处理
    增加模糊搜索，增加checkbox搜索

---
+++ models/restaurants.js
@@ -0,0 +1,22 @@
+var mongoose = require('mongoose');
+
+var Schema = mongoose.Schema;
+
+var Restaurant = new Schema(
+    {
+        restaurant_name:{type: String, required: true, max:100},
+        class_name:{type: String, required: true, max:100},
+        special_name:{type: String, required: true, max:100},
+        locate_longitude:{type: String, required: true, max:100},
+        locate_latitude:{type: String, required: true, max:100},
+        post_code:{type: String, required: true, max:100},
+        rank_score:{type: String, required: true, max:100},
+        level_score:{type: String},
+        logo_image:{type: String},
+        whatever: {type: String} //any other field
+    }
+);
+
+var characterModel = mongoose.model('Restaurant', Restaurant );
+
+module.exports = characterModel;
